4010f3d2cdb5c74db3e61d764cfe8bcee6068a38
SashaRPG <uasasharpg@gmail.com>
2022-04-23 22:57:05 +0300

    feat: added some functionality to burger menu

--- scripts/burger_menu.js
+++ scripts/burger_menu.js
@@ -1,28 +1,41 @@
 'use strict'
 
+function toggleActiveClass(classNameBlock,toggleClassName) {
+    const block = document.querySelector(classNameBlock);
+    if(block.classList.contains(toggleClassName)) block.classList.remove(toggleClassName)
+    else block.classList.add(toggleClassName)
+}
+function deleteActiveClass(classNameBlock,toggleClassName) {
+    const block = document.querySelector(classNameBlock);
+    if(block.classList.contains(toggleClassName)) block.classList.remove(toggleClassName)
+}
+function addActiveClass(classNameBlock,toggleClassName) {
+    const block = document.querySelector(classNameBlock);
+    if(!block.classList.contains(toggleClassName)) block.classList.add(toggleClassName)
+}
+
 function activateMenu() {
     const button = document.querySelector('.burger-menu');
     const overlay = document.querySelector('.overlay');
-
-    function toggleActiveClass(classNameBlock,toggleClassName) {
-        const block = document.querySelector(classNameBlock);
-        if(block.classList.contains(toggleClassName)){
-            block.classList.remove(toggleClassName);
-        } else{
-            block.classList.add(toggleClassName);
-        } 
-    }
+    const nav_items = document.querySelectorAll('.nav-list a');
     button.addEventListener('click', () => {
         toggleActiveClass('.navigation', 'navigation-active');
         toggleActiveClass('.burger-menu', 'burger-menu-active');
         toggleActiveClass('.overlay', 'active');
-        toggleActiveClass('.shelter-logo', 'active');
+        toggleActiveClass('body', 'active');
     })
     overlay.addEventListener('click', (e) => {
-        toggleActiveClass('.navigation', 'navigation-active');
-        toggleActiveClass('.burger-menu', 'burger-menu-active');
-        toggleActiveClass('.overlay', 'active');
+        deleteActiveClass('.navigation', 'navigation-active');
+        deleteActiveClass('.burger-menu', 'burger-menu-active');
+        deleteActiveClass('.overlay', 'active');  
+        deleteActiveClass('body', 'active'); 
+        closePopup();     
     })
+    nav_items.forEach(item => item.addEventListener('click', () => {
+        deleteActiveClass('.navigation', 'navigation-active');
+        deleteActiveClass('.burger-menu', 'burger-menu-active');
+        deleteActiveClass('.overlay', 'active');  
+        deleteActiveClass('body', 'active'); 
+    }))
 }
-activateMenu()
-
+activateMenu();
---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const btn_left = document.querySelector('.btn-left');
+const btn_right = document.querySelector('.btn-right');
+
+const our_friends_slider = document.querySelector('.our-friends-slider');
+
+btn_left.addEventListener('click', )
